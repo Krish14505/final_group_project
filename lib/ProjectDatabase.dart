9caf0a54e5c541047ee503adff1af3d171dec0e8
Krish Chaudhary <161269571+Krish14505@users.noreply.github.com>
2024-08-08 19:20:41 -0400

    created the Database Class to have only and only one database

---
+++ lib/ProjectDatabase.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:floor/floor.dart';
+import 'package:floor_annotation/floor_annotation.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+
+import 'Customer.dart';
+import 'CustomerDAO.dart';
+import 'Flight.dart';
+import 'FlightsDAO.dart';
+import 'Reservation.dart';
+import 'ReservationDAO.dart';
+import 'airplane.dart';
+import 'airplane_dao.dart';
+
+part 'CustomerDatabase.g.dart';
+
+///Customer Database class extends FloorDatabase
+
+@Database(version: 1, entities: [Customer,Reservation,Flight,Airplane])
+abstract class ProjectDatabase extends FloorDatabase {
+
+  ///get the interface ready to database
+  CustomerDAO get getCustomerDAO; // function to establish the connection.
+
+  ///get the interface Airplane ready to Airplane
+  AirplaneDao get getAirplaneDAO;
+
+  ///get the interface ready for the reservation
+  ReservationDAO get getReservationDAO;
+
+  ///get the interface ready for the Flight
+  FlightsDAO get getFlightDAO;
+
+}
